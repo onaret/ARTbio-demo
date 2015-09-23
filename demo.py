f87ec9c4b3ea5d1cbb29a10d9807841a5a24fcb2
onaret <onaret@gmail.com>
2015-09-23 18:28:02 +0200

    take input and output files as argument. parse input file to generate output containing sequences content and the numbers of sequences.

--- demo.py
+++ demo.py
@@ -1,9 +1,23 @@
 """
 Discovering biopython, using code from http://biopython.org/wiki/SeqIO
 """
-
+import argparse
 from Bio import SeqIO
-handle = open("sample.fasta", "rU")
-for record in SeqIO.parse(handle, "fasta") :
-    print record.id
-handle.close()
+
+parser = argparse.ArgumentParser(description='Takes in input any Fasta file and returns in a "results" file \n a) the number of sequences in the file b) \n a tabular list of sequences name \\t sequence length')
+parser.add_argument('--input', required=True, help='Input fasta file')
+parser.add_argument('--output', required=False, default = 'results.txt', help='Result file')
+param = parser.parse_args()
+
+input_handle = open(param.input, "rU")
+output_handle = open(param.output, "w")
+
+count=0
+for record in SeqIO.parse(input_handle, "fasta") :
+    count += SeqIO.write(record, output_handle, "fasta")
+    output_handle.write("\t")
+
+output_handle.write('There is ' + str(count) + " sequences in file " + str(param.input))
+
+input_handle.close()
+output_handle.close()
